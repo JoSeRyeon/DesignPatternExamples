0bb82c9fb12176498cb7febdcced880271ca533f
JoSeRyeon <66010019+JoSeRyeon@users.noreply.github.com>
2020-05-28 22:50:23 +0900

    Add files via upload

---
+++ DesignPatternExamples/src/com/example/designpattern/composite/Headset.java
@@ -0,0 +1,16 @@
+package com.example.designpattern.composite;
+
+public class Headset extends ComputerDevice{
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPower() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
